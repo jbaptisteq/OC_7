82f1b8be58c998d1504acd05e52a5f5b003778f7
jbaptiste <jb.queralt@gmail.com>
2018-11-01 18:01:00 +0100

    feat : #9 UserController - Create controller - Create function newUser - Update route.yaml - Delete Authcontroller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,20 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use App\Entity\User;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Entity\Client;
 
-class AuthController extends AbstractController
+class UserController extends AbstractController
 {
-    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    public function newUser(Request $request, UserPasswordEncoderInterface $encoder)
     {
         $em = $this->getDoctrine()->getManager();
+        $repository = $this->getDoctrine()->getRepository(Client::class);
 
         $username = $request->request->get('_username');
         $password = $request->request->get('_password');
+        $id = $request->request->get('_client');
+        $client = $repository->findOneBy(['id' => $id]);
 
-        $user = new User($username);
+        $user = new User();
+        $user->setUsername($username);
         $user->setPassword($encoder->encodePassword($user, $password));
+        $user->setClient($client);
         $em->persist($user);
         $em->flush();
 
