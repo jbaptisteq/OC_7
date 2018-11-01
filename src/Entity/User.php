54fec76a2d7993825de699ccc21a2f50f76912a4
jbaptiste <jb.queralt@gmail.com>
2018-11-01 17:39:09 +0100

    fix : Entity User
    - bug in getter resolve
    - addind serialization groups
    - configure opération

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,15 +3,31 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Serializer\Annotation\Groups;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiSubresource;
 
 /**
 * @ORM\Table(name="users")
 * @ORM\Entity
 *
-* @ApiResource()
+* @ApiResource(
+*   collectionOperations={
+*       "get",
+*       "post"={
+*           "method"="POST",
+*           "path"="/users",
+*           "controller"=UserController::Class}
+*       },
+*   itemOperations={
+*       "get",
+*        "delete"},
+**   normalizationContext={"groups"={"read"}}
+* )
 */
 class User implements UserInterface
 {
@@ -19,12 +35,14 @@ class User implements UserInterface
     * @ORM\Column(type="integer")
     * @ORM\Id
     * @ORM\GeneratedValue(strategy="AUTO")
+    * @Groups("read")
     */
     private $id;
 
     /**
     * @ORM\Column(type="string", length=25, unique=true)
     * @Assert\NotBlank
+    * @Groups("read")
     */
     private $username;
 
@@ -35,19 +53,26 @@ class User implements UserInterface
 
     /**
     * @ORM\Column(name="is_active", type="boolean")
+    * @Groups("read")
     */
     private $isActive;
 
     /**
-    * @ORM\ManyToOne(targetEntity="App\Entity\Client", inversedBy="users")
-    * @ORM\JoinColumn(nullable=false)
+    * @ORM\ManyToOne(targetEntity="Client", inversedBy="users")
+    * @ORM\JoinColumn(nullable=false, referencedColumnName="id")
     * @Assert\NotBlank
+    * @Groups("read")
     */
     private $client;
 
     public function __construct()
     {
         $this->isActive = true;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
     }
 
     public function getUsername()
@@ -75,12 +100,17 @@ class User implements UserInterface
         $this->password = $password;
     }
 
-    public function getClient(): ?Client
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    public function getClient(): Client
     {
         return $this->client;
     }
 
-    public function setClient(?Client $client): self
+    public function setClient(Client $client): self
     {
         $this->client = $client;
 
